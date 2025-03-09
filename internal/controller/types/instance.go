1b671a11e95cfc43e1d711b123def6aac6ed20ca
Adalberto Junior <adalberto.gmu@gmail.com>
2025-03-09 13:00:14 -0300

    add type for pipeline handle funcs

--- internal/controller/types/instance.go
+++ internal/controller/types/instance.go
@@ -13,14 +13,3 @@ const (
 func (p Property) String() string {
 	return string(p)
 }
-
-type CustomProviderTags string
-
-const (
-	CUSTOM_PROVIDER_KEY   CustomProviderTags = "ManagedBy"
-	CUSTOM_PROVIDER_VALUE CustomProviderTags = "Crossplane-compute-provider"
-)
-
-func (p CustomProviderTags) String() string {
-	return string(p)
-}
